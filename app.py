c375442d984f18bbc985902c2a1b7a02a6da94bc
Christina Gomez <christina.gomez27@gmail.com>
2019-02-20 22:37:51 -0500

    modified models and app files for Postgres database

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://shhh-auction:$al3mr3!gn$FEB@localhost:8889/shhh-auction'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://christina:r3!gn$AL3M@localhost/shhh_auction'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '$al3mr3!gn$FEB'
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import app, db
+from datetime import datetime
 
 class User(db.Model):
 
@@ -17,8 +18,8 @@ class Item(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    start = db.Column(db.String(120))
-    stop = db.Column(db.String(120))
+    start = db.Column(db.DateTime, nullable = False)
+    stop = db.Column(db.DateTime, nullable = False)
     description = db.Column(db.String(455))
     bids = db.relationship('Bid', backref='item-bids', lazy='dynamic')
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -35,13 +36,15 @@ class Bid(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.title'))
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime,  default=db.func.current_timestamp())
 
-    def __init__(self, amount, item_id, owner_id):
+    def __init__(self, amount, item_id, owner_id, timestamp):
         self.amount = amount
         self.item_id = item_id
         self.owner_id = owner_id
+        self.timestamp = timestamp
 
 
         
