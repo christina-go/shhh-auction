f687eb1ae486c49fd1a6e4f912b4d3f18a1f90d2
christina-go <42299009+christina-go@users.noreply.github.com>
2024-06-12 11:52:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://olzatrjzhhxuon:2cc542237dc6b08ee996a94181bc972144e0dce38b2b867c2140c2aecad3059b@ec2-54-204-41-109.compute-1.amazonaws.com/d6n7s0roossktr'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '$al3mr3!gn$FEB'
+#app.secret_key = 
 
 UPLOAD_FOLDER = './static/users/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
